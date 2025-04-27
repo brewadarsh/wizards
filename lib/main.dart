83efa24884c75f1c618c16c96b836ff5b584e779
brewadarsh <brewadarsh@gmail.com>
2025-04-27 19:23:19 +0530

    Hogwarts container added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'apps/hogwarts_app.dart';
 import 'package:flutter/cupertino.dart';
 
+import 'layers/core/utils/local_storage.dart';
+import 'layers/hogwarts_container.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  LocalStorageSync.instance.init();
+  HogwartsContainer.inject();
   runApp(const HogwartsApp());
 }
